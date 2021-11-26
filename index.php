e08201a0f9a51ccc09c55af9bf67a9fd3629c6c2
DaniGavli <dngg55@gmail.com>
2021-11-26 19:17:02 -0300

    Front_end tela vendas login menu e cadastro

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+     <style>
+
+  body {background: linear-gradient(90deg, #00BFFF 65%, #FFFFFF 35%);}
+  label {color: 00BFFF; font-size:22px; marging-right: 8px;}
+  form { text-align:center; position:absolute; left: 70%; top: 35%;}
+  input{ width:250px; height: 25px; marging-top: 3px; marging-left: 20px;}
+  #btn {width:150px; height: 35px; marging-top: 6px; marging-left: 20%; background-color: #FFFFFF; color: #00BFFF; border: none;}
+  div{font-size: 50px; color: #ffffff; position:absolute; left: 20%; top: 25%; }
+  
+  </style>
+        <div>Loja xxx</div>
+  
+  <form method="post" action="telaMenu.html">
+
+  <label>Login: <input type="text" name="id_func" placeholder="Sua Matricula"></label><br><br>
+   <label>Senha: <input type="text" name="senha_func" placeholder="Senha"></label><br><br>
+  <input id="btn" type="submit" value="ENTRAR" name="entrar"><br>
+  
+</form>
+    </body>
+</html>
